21c8642d5431e4d6e2339193d652840b48c67f60
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-11-29 18:36:38 +0100

    creazione main

---
+++ src/components/AppMainCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "AppMainCard",
+  props: {
+    thumb: "",
+    series: "",
+  },
+};
+</script>
+
+<template>
+  <div class="flex column">
+    <img :src="thumb" alt="" />
+    <span>{{ series }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+  width: 6.25rem;
+  height: 6.25rem;
+}
+</style>
